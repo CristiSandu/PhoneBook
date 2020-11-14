41aee239a03b9608bce5aa19c495e2b0d64d0f84
cristysandu <crystisandu3@gmail.com>
2020-11-14 21:16:01 +0200

    add project with first interface

---
+++ AgendaTelefonica/AgendaTelefonica/AgendaTelefonica/PageCont/DisContact.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using AgendaTelefonica.PageCont;
+
+namespace AgendaTelefonica.PageCont
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class DisContact : ContentPage
+    {
+        public DisContact()
+        {
+            InitializeComponent();
+        }
+
+        private async void backBtn_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PopModalAsync();
+        }
+    }
+}
---
+++ AgendaTelefonica/AgendaTelefonica/AgendaTelefonica/PagesTab/History.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using AgendaTelefonica.PageCont;
+
+namespace AgendaTelefonica.PagesTab
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class History : ContentPage
+    {
+        public History()
+        {
+            InitializeComponent();
+        }
+
+        private async void dispContact_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushModalAsync(new DisContact());
+        }
+    }
+}
