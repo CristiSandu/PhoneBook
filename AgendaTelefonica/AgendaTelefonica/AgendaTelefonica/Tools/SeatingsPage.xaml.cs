b323bab5d32279753f3c0bec3135be6562e80bb6
cristysandu <crystisandu3@gmail.com>
2021-04-18 21:33:34 +0300

    fix microchart view

--- AgendaTelefonica/AgendaTelefonica/AgendaTelefonica/Tools/SeatingsPage.xaml.cs
+++ AgendaTelefonica/AgendaTelefonica/AgendaTelefonica/Tools/SeatingsPage.xaml.cs
@@ -14,38 +14,9 @@ namespace AgendaTelefonica.Tools
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class SeatingsPage : ContentPage
     {
-        private readonly ChartEntry[] entries = new[]
-        {
-             new ChartEntry(212)
-             {
-                 Label = "UWP",
-                 ValueLabel = "212",
-                 Color = SKColor.Parse("#2c3e50")
-             },
-             new ChartEntry(248)
-             {
-                 Label = "Android",
-                 ValueLabel = "248",
-                 Color = SKColor.Parse("#77d065")
-             },
-             new ChartEntry(128)
-             {
-                 Label = "iOS",
-                 ValueLabel = "128",
-                 Color = SKColor.Parse("#b455b6")
-             },
-             new ChartEntry(514)
-             {
-                 Label = "Shared",
-                 ValueLabel = "514",
-                 Color = SKColor.Parse("#3498db")
-             }
-        };
-        
         public SeatingsPage()
         {
             InitializeComponent();
-            chartView.Chart = new RadarChart { Entries = entries };
         }
     }
 }
